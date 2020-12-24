26d28f46289c43ab9f94ff28c2ee795c8444c494
Tiffany Crockett <crockett.tiff@gmail.com>
2020-12-24 16:26:00 -0700

    js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,12 +8,13 @@ $(document).ready(function(){
     
     // set 12 hour time as work hours for each time block
     var times = [9, 10, 11, 12, 13, 14, 15, 16, 17]; 
-
-    for(var i = 0; i < times.length; i++) {
-             workTime = moment(times.toString(), "LT").format("hh:mm A"); 
+ 
+        times.forEach(times =>
+            workTime = moment(times.toString(), "LT").format("hh:mm A")); 
                 $("#time-"+ i).text(workTime); 
+                console.log(times)
                 console.log(workTime);
-
+        
             var workTime24Hr = moment(times.toString(), "LT").format("HH"); 
                 console.log(workTime24Hr); 
                 console.log(time24Hr);
@@ -25,7 +26,7 @@ $(document).ready(function(){
             } else if (time24Hr > workTime24Hr) {
                 $("#text-" + i).css({"background-color":"#d3d3d3", "color":"white"})
             }   
-        }; 
+        
 
     var workHour = [9, 10, 11, 12, 1, 2, 3, 4, 5];
     // key, each work hour  val, input text - combine to local storage
